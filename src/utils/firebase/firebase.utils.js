549a645426725804122435ffde6c82896a495251
FredNeves95 <fredbneves95@gmail.com>
2022-08-01 17:17:49 -0300

    add firebase and signin with google

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,51 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAXg6m4xqeaskPfrUdjgz0HUZpJG8G7iPc",
+    authDomain: "cap-n-shirt-db.firebaseapp.com",
+    projectId: "cap-n-shirt-db",
+    storageBucket: "cap-n-shirt-db.appspot.com",
+    messagingSenderId: "286009830986",
+    appId: "1:286009830986:web:0d2ba56ed309086d5100b3",
+    measurementId: "G-31BMYNJSC6"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Check documentation for analythics
+// const analytics = getAnalytics(firebaseApp);
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName, email, createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef
+}
