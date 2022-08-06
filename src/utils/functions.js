32a39aa73e01c0cfdaf5f914517491be7ffbbd80
FredNeves95 <fredbneves95@gmail.com>
2022-08-06 13:02:47 -0300

    fix getroute logic

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,6 @@
 export const getRoute = (data, conditionalRoutes) => {
     const routesArray = Object.values(conditionalRoutes)
-    if (data || data?.length) return routesArray[0];
+    if (data?.length) return routesArray[0];
+    if (data && data.length !== 0) return routesArray[0];
     return routesArray[1];
 };
