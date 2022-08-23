a898df48d0cc1039f4513019608ef0a1ef6a1041
FredNeves95 <fredbneves95@gmail.com>
2022-08-23 19:09:10 -0300

    configure redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export const store = configureStore({ reducer: rootReducer, middleware: [logger] })
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({ reducer: persistedReducer, middleware: [process.env.NODE_ENV === 'development' && logger].filter(Boolean) })
+
+export const persistor = persistStore(store)
