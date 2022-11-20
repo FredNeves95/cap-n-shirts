662dd0dd72e94a27244795d2b1f1355daa290958
FredNeves95 <fredbneves95@gmail.com>
2022-11-20 18:56:32 -0300

    makes app a PWA

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Elements } from '@stripe/react-stripe-js'
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { Provider } from 'react-redux'
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
@@ -26,7 +27,7 @@ root.render(
 
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
